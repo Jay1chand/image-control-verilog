1e94f7cacec26792ca4487df2fefa2be11c671c0
Jay1chand <135565886+Jay1chand@users.noreply.github.com>
2024-05-24 08:02:56 +0530

    Update imgencr_tb.v

--- imgencr_tb.v
+++ imgencr_tb.v
@@ -1,54 +1,62 @@
 `timescale 1ns/1ns
 `include "imgencr.v"
-module imgencr_tb();
-reg[0:7] ibyte;
-wire [0:7] obyte;
-reg clk;
+module testbench_1();
+reg [0:7] inbyte;
 reg [0:7] threshold;
 reg [0:7] value;
 reg [0:1] select;
+reg clk ;
+wire [0:7] outbyte;
 integer i=0;
 integer fd;
-reg [0:7] data_in [0:250000];
-reg [0:7] data_out[0:250000];
-imgencr uut (obyte,ibyte,threshold,value,select,clk);
-initial
-begin
-    fd=$open("output.txt","w");
-    clk=1'b0;
-    $readmeh("image_teta.txt",data_in);
+reg [0:7] data_in [0:102399];
+reg [0:7] data_out [0:102399];
+
+pixel_operator po1(outbyte,inbyte,threshold,value,select,clk);
+
+initial 
+begin 
+// code for the output file 
+fd=$fopen("outputa.txt","w");
+//instantiation clk 
+clk = 1'b0;
+// this is for reading the input text file 
+$readmemh("ichip22_ps1_image_text.txt", data_in);
 end
 
-always
-begin
-
-#10 clk=~clk;
+always 
+begin 
+#10 clk=~clk ;
 end
 
-initial
-begin
-
+initial 
+begin 
 i=0;
 value=60;
-threshold=120;
+threshold=160;
 select=3;
+//looping through all data
 
-while (i<250000) begin
+while(i<102399)
 
-ibyte = data_in[i]; // Accessing data_in[i] as a 1D array
-
+begin
+inbyte=data_in[i];
 #20
-data_out[i]=obyte;
+data_out[i]=outbyte;
+i=i+1;
+
+end
+
+i=0;
+//looping for saving all data
+while(i<102399)
+begin
+$fwriteh(fd,data_out[i]);
+$fwrite(fd," ");
 i=i+1;
 end
-i=0;
-while(i<250000)
-begin
-    $fwriteh(fd,data_out[i]);
-    $fwrite(fd," ");
-    i=i+1;
-end
+ 
 $finish;
-    
 end
+
 endmodule
